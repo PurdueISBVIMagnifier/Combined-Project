6d2e804d674bd78e822f0db8fb2e9d3551bf664b
Meriem <mbounab@purdue.edu>
2019-03-27 10:42:43 -0400

    fixed texted resizing

--- UI-SOURCES/MenuController.swift
+++ UI-SOURCES/MenuController.swift
@@ -4,7 +4,20 @@ class MenuController: UIViewController
 {
     //VAR DECLARATIONS
     var ipText: String? //The string that is passed to the select device controller
-    
+    @IBOutlet weak var fsButton: UIButton!
+    @IBOutlet weak var umButton: UIButton!
+    @IBOutlet weak var ssButton: UIButton!
+    @IBOutlet weak var rvButton: UIButton!
+    override func viewDidLoad() {
+        fsButton.titleLabel?.minimumScaleFactor = 0.5;
+        fsButton.titleLabel?.adjustsFontSizeToFitWidth = true;
+        umButton.titleLabel?.minimumScaleFactor = 0.5;
+        umButton.titleLabel?.adjustsFontSizeToFitWidth = true;
+        ssButton.titleLabel?.minimumScaleFactor = 0.5;
+        ssButton.titleLabel?.adjustsFontSizeToFitWidth = true;
+        rvButton.titleLabel?.minimumScaleFactor = 0.5;
+        rvButton.titleLabel?.adjustsFontSizeToFitWidth = true;
+    }
     @IBAction func returntoMain(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
