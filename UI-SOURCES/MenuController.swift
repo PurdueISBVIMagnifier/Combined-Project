411824a2f64f2970dd194d1976f0d15a1cac10db
Meriem <mbounab@purdue.edu>
2019-03-27 10:30:54 -0400

    Revert "added label resizing"
    This reverts commit a93c7388ffc085ce92b7a81ecf7cea7e962cb126.

--- UI-SOURCES/MenuController.swift
+++ UI-SOURCES/MenuController.swift
@@ -4,19 +4,7 @@ class MenuController: UIViewController
 {
     //VAR DECLARATIONS
     var ipText: String? //The string that is passed to the select device controller
-    @IBOutlet weak var fsButton: UIButton!
-    @IBOutlet weak var umButton: UIButton!
-    @IBOutlet weak var ssButton: UIButton!
-    @IBOutlet weak var rvButton: UIButton!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        fsButton.titleLabel?.adjustsFontSizeToFitWidth=true
-        umButton.titleLabel?.adjustsFontSizeToFitWidth=true
-        ssButton.titleLabel?.adjustsFontSizeToFitWidth=true
-        rvButton.titleLabel?.adjustsFontSizeToFitWidth=true
-    }
     @IBAction func returntoMain(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
